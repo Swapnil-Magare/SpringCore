0798d21c80c5233637ef64ec8d4748d0ccc18ded
Swapnil Magare <swapnilmagare0907@gmail.com>
2024-12-17 19:39:04 +0530

    IOC container initialized successfully.

---
+++ 1IOC_containers/src/main/java/IOC_Containers_Basic/Person.java
@@ -0,0 +1,10 @@
+package IOC_Containers_Basic;
+
+public class Person {
+	public Person(){
+		System.out.println("Person Object is Created");
+	}
+	public void run() {
+		System.out.println("Person is Running fast!!!!");
+	}
+}
