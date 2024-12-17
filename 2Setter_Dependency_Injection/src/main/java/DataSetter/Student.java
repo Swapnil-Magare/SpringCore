ec6d7164625ed7b5eb43b3aa0f9f3f5d9c36744e
Swapnil Magare <swapnilmagare0907@gmail.com>
2024-12-17 19:41:25 +0530

    Setter injection completed. Dependencies set successfully.

---
+++ 2Setter_Dependency_Injection/src/main/java/DataSetter/Student.java
@@ -0,0 +1,26 @@
+package DataSetter;
+
+public class Student {
+	private int sid;
+	private String name;
+	
+	public void Bunk() {
+		System.out.println("Bunk is done!!");
+	}
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ 2Setter_Dependency_Injection/src/main/java/ObjectSetter/Mobile.java
@@ -0,0 +1,17 @@
+package ObjectSetter;
+
+public class Mobile {
+	 private String Brand;
+	 
+	 public String getBrand() {
+		return Brand;
+	}
+
+	public void setBrand(String brand) {
+		Brand = brand;
+	}
+
+	public void ring() {
+		 System.out.println("Mobile is Ringing");
+	 }
+}
